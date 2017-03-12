8e6cb41ec287eb3ef44e1990bef6a13dd5abf4d7
Vadorequest <ambroise.dhenain@gmail.com>
2017-03-12 22:58:06 +0100

    Add more images

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Carousel from './Carousel';
 
 ReactDOM.render(
   <Carousel
-    backgroundColor="pink"
     slides={[
       {
         id: '1',
@@ -44,6 +43,66 @@ ReactDOM.render(
               },
               {
                 url: 'http://static.milibris.com/carousel/sfr-presse/resources/140cca96-4371-4579-abd0-c142262a33d4',
+              },
+            ],
+          }
+        ]
+      },
+      {
+        id: '3',
+        action: {
+          type: 'link',
+          value: 'http://google2.com',
+        },
+        sizes: [
+          {
+            size: 'large',
+            images: [
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/394de467-2479-416f-b0b2-d18cc805f48b",
+              },
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/833e4598-4f40-42f3-a994-8027106f48a6",
+              },
+            ],
+          }
+        ]
+      },
+      {
+        id: '4',
+        action: {
+          type: 'link',
+          value: 'http://google2.com',
+        },
+        sizes: [
+          {
+            size: 'large',
+            images: [
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/a0b1305b-3442-4852-ae33-bcb9949fc5cf",
+              },
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/222839ef-f177-441b-98df-77fdd0773dcd",
+              },
+            ],
+          }
+        ]
+      },
+      {
+        id: '5',
+        action: {
+          type: 'link',
+          value: 'http://google2.com',
+        },
+        sizes: [
+          {
+            size: 'large',
+            images: [
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/6c7ce2fe-2c3f-4fb5-8f58-7aa3d40541bf",
+              },
+              {
+                url: "http://static.milibris.com/carousel/sfr-presse/resources/814b8246-d894-4888-b2ba-96da186a176f",
               },
             ],
           }
